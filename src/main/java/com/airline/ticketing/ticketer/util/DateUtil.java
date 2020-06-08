e594cb7b9317b6bbc8f28ca4aa0f4af38d646aa1
Mahmut <mahmut.bogaz@partner.turktelekom.com.tr>
2020-06-08 22:48:26 +0300

    dateutil toFlightDate added

---
+++ src/main/java/com/airline/ticketing/ticketer/util/DateUtil.java
@@ -0,0 +1,22 @@
+package com.airline.ticketing.ticketer.util;
+
+import org.omg.CORBA.BAD_PARAM;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateUtil {
+
+    public static Date toFlightDate(String dateStr) {
+        String dateFormat = "dd-MM-yyyy";
+        SimpleDateFormat sdf1 = new SimpleDateFormat(dateFormat); // New Pattern
+        java.util.Date date = null; // Returns a Date format object with the pattern
+        try {
+            date = sdf1.parse(dateStr);
+        } catch (ParseException e) {
+            throw new BAD_PARAM("Date format must be : " + dateFormat);
+        }
+        return new java.sql.Date(date.getTime());
+    }
+}
