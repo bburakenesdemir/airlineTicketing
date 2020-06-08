67af3c3d2c2c309a1d5ae6bc5fb03d7fbd108bb1
Mahmut <mahmut.bogaz@partner.turktelekom.com.tr>
2020-06-08 16:26:45 +0300

    Spring Boot base

---
+++ src/main/java/com/airline/ticketing/ticketer/TicketerApplication.java
@@ -0,0 +1,13 @@
+package com.airline.ticketing.ticketer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketerApplication.class, args);
+    }
+
+}
