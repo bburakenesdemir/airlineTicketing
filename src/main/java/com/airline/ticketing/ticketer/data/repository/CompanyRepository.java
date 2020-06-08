22d339f70b7c71963ec8c13cb3668d85d9bbdd2b
Mahmut <mahmut.bogaz@partner.turktelekom.com.tr>
2020-06-08 20:04:43 +0300

    company pageable

--- src/main/java/com/airline/ticketing/ticketer/data/repository/CompanyRepository.java
+++ src/main/java/com/airline/ticketing/ticketer/data/repository/CompanyRepository.java
@@ -1,9 +1,14 @@
 package com.airline.ticketing.ticketer.data.repository;
 
 import com.airline.ticketing.ticketer.data.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    Page<Company> findAllByNameContainsOrDescContains(String namePart, String DescPart, Pageable pageable);
+
 }
--- src/main/java/com/airline/ticketing/ticketer/service/CompanyService.java
+++ src/main/java/com/airline/ticketing/ticketer/service/CompanyService.java
@@ -3,11 +3,13 @@ package com.airline.ticketing.ticketer.service;
 import com.airline.ticketing.ticketer.data.Company;
 import com.airline.ticketing.ticketer.data.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CompanyService extends EntityService<Company>{
+public class CompanyService extends EntityService<Company> {
 
     @Autowired
     private CompanyRepository companyRepository;
@@ -15,5 +17,10 @@ public class CompanyService extends EntityService<Company>{
     @Override
     public JpaRepository<Company, Long> getRepository() {
         return companyRepository;
+    }
+
+    @Override
+    public Page<Company> pageableSearch(Pageable pageable, String text) {
+        return companyRepository.findAllByNameContainsOrDescContains(text, text, pageable);
     }
 }
